36019d14d8cd6d1336ff85de8ffef8e58674bdad
William <will_sun@y7mail.com>
2022-04-30 14:09:12 +1000

    First commit for GUI

---
+++ youtube-dl-gui/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Dynamic;
+
+namespace youtube_dl_gui.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public string Text { get; set; }
+
+
+        public MainWindowViewModel()
+        {
+            Text = "hello";
+        }
+
+
+    }
+}
