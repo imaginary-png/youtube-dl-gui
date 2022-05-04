542b20f10e1acca3b8c4e04f0c0c63b988d6a999
William <will_sun@y7mail.com>
2022-05-04 19:02:45 +1000

    Added settings classes

---
+++ youtube-dl-gui/Models/Settings.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+using Newtonsoft.Json;
+using youtube_dl_gui_wrapper.Models;
+
+namespace youtube_dl_gui.Models
+{
+    public class Settings : ObservableObject
+    {
+        private bool _useYoutubeDl;
+        private bool _bulkDownload;
+        private string _outputFolder;
+
+        /// <summary>
+        /// True uses youtube-dl, false uses yt-dlp. Defaults to False.
+        /// </summary>
+        [DefaultValue(false)]
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
+        public bool UseYoutubeDL
+        {
+            get => _useYoutubeDl;
+            set
+            {
+                if (value == _useYoutubeDl) return;
+                _useYoutubeDl = value;
+                OnPropertyChanged(nameof(UseYoutubeDL));
+            }
+        }
+
+        /// <summary>
+        /// True downloads concurrently, false downloads one-by-one. Defaults to True.
+        /// </summary>
+        [DefaultValue(true)]
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
+        public bool BulkDownload
+        {
+            get => _bulkDownload;
+            set
+            {
+                if (value == _bulkDownload) return;
+                _bulkDownload = value;
+                OnPropertyChanged(nameof(BulkDownload));
+            }
+        }
+
+        /// <summary>
+        /// Output Folder for downloads. defaults to desktop
+        /// </summary>
+        [DefaultValue(@"%USERPROFILE%\Desktop\")]
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Populate)]
+        public string OutputFolder
+        {
+            get => _outputFolder;
+            set
+            {
+                if (value == _outputFolder) return;
+                _outputFolder = value;
+                OnPropertyChanged(nameof(OutputFolder));
+            }
+        }
+
+        public Settings()
+        {
+            UseYoutubeDL = false;
+            BulkDownload = true;
+            OutputFolder = @"%USERPROFILE%\Desktop\";
+        }
+
+        public Settings(bool useYoutubeDl, bool bulkDownload, string outputFolder)
+        {
+            _useYoutubeDl = useYoutubeDl;
+            _bulkDownload = bulkDownload;
+            _outputFolder = outputFolder;
+        }
+    }
+}
---
+++ youtube-dl-gui/Models/SettingsManager.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using youtube_dl_gui_wrapper.Models;
+
+namespace youtube_dl_gui.Models
+{
+    public class SettingsManager : ObservableObject
+    {
+        private const string ConfigFileLocation = "./config.json";
+
+        public Settings UserSettings;
+
+        public SettingsManager()
+        {
+            LoadSettings();
+        }
+
+        public void LoadSettings()
+        {
+            if (File.Exists(ConfigFileLocation))
+            {
+                var jsonSettings = new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    MissingMemberHandling = MissingMemberHandling.Ignore
+                };
+
+                UserSettings =
+                    JsonConvert.DeserializeObject<Settings>(File.ReadAllText(ConfigFileLocation), jsonSettings);
+                return;
+            }
+
+            CreateDefaultSettings();
+        }
+
+        public void SaveSettings()
+        {
+            File.WriteAllText(ConfigFileLocation, JsonConvert.SerializeObject(UserSettings, Formatting.Indented));
+            OnPropertyChanged(nameof(UserSettings)); //not needed?
+        }
+
+
+        private void CreateDefaultSettings()
+        {
+            File.Create(ConfigFileLocation).Close();
+            UserSettings = new Settings();
+            SaveSettings();
+        }
+
+
+    }
+}
+
