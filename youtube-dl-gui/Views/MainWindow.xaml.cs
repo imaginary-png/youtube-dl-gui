e6e3385d805ccf44a0d3d1b9fdfb00f15887c114
William <will_sun@y7mail.com>
2022-05-06 16:06:17 +1000

    Prettied up gui, added images, styling, consistent colour theme

--- youtube-dl-gui/Views/MainWindow.xaml.cs
+++ youtube-dl-gui/Views/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace youtube_dl_gui.Views
 {
@@ -8,6 +10,9 @@ namespace youtube_dl_gui.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string _sidePanelFocusColour = "#202225";
+        private string _sidePanelUnfocusColour = "#2f3136";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -19,6 +24,46 @@ namespace youtube_dl_gui.Views
             {
                 DragMove();
             }
+        }
+
+        private void MinimizeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Maximizebutton_OnClick(object sender, RoutedEventArgs e)
+        {
+            AdjustSize();
+        }
+
+
+        private void AdjustSize()
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void CloseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void DownloadPageButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            SettingsPageSelectedBar.Visibility = Visibility.Hidden;
+            DownloadPageSelectedBar.Visibility = Visibility.Visible;
+        }
+
+        private void SettingsPageButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            DownloadPageSelectedBar.Visibility = Visibility.Hidden;
+            SettingsPageSelectedBar.Visibility = Visibility.Visible;
         }
     }
 }
